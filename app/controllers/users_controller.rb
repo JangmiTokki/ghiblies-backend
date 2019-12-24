a2f55dbd9ec9e5a066fbd13c2a139412b650b73a
github username <13933554+JangmiTokki@users.noreply.github.com>
2019-12-24 12:34:15 -0700

    added users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render status: :ok
+        else
+            render json: { error: "User does not exist"}, status: :bad_request 
+        end
+    end
+
+    private 
+    
+    def user_params
+        params.require(:username, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_digest
+    validates :username, presence: true
+    validates :password, presence: true, length: {minimum: 8}
 end
